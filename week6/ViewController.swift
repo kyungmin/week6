bb3f8592276f9576fe2bb96ead00a82c23fd7460
Kyungmin Kim <kmin905@gmail.com>
2015-03-09 19:12:30 -0700

    Initial Commit

---
+++ week6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week6
+//
+//  Created by Kyungmin Kim on 3/9/15.
+//  Copyright (c) 2015 Kyungmin Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
